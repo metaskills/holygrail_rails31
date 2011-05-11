eec414c75a356573e2db4d47ab2dc542220af5c4
Ken Collins <ken@metaskills.net>
2011-05-10 20:36:52 -0400

    New Rails 3.1.0 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HolygrailRails31::Application.config.secret_token = '5e6c60a8d3ad0942072c7c58ccaaacfd8bfe3de63ed3a5c1fe33138607e29999b11c2da7ddf561a92344f82432567d3bb4e1b5e45d1db9f1e09c55d99d1254d0'
